7c6cb95c3f461111864231403ef1c88962b80284
yuval <Arbelyuval56@gmail.com>
2022-12-31 18:09:14 +0200

    new functions in socketclient: 1. connecting 2. send to server 3. recieve from server.

--- Client/ClientSocket.cpp
+++ Client/ClientSocket.cpp
@@ -36,9 +36,9 @@ int ClientSocket::getPort() {
  */
 int ClientSocket::creatClientSocket() {
     // Create a new socket.
-    int serverSocket = makeNewSocket();
+    int clientSocket = makeNewSocket();
     struct sockaddr_in sin = creatAddrInStruct();
-    return serverSocket;
+    return clientSocket;
 }
 
 /**
@@ -47,12 +47,12 @@ int ClientSocket::creatClientSocket() {
  */
 int ClientSocket::makeNewSocket() {
     // Creat a new socket.
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     // If the creation didn't work, raise an error.
-    if (serverSocket < 0) {
+    if (clientSocket < 0) {
         perror("Error binding socket.");
     }
-    return serverSocket;
+    return clientSocket;
 }
 
 /**
@@ -74,5 +74,58 @@ struct sockaddr_in ClientSocket::creatAddrInStruct() {
     sin.sin_port = htons(serverPort);
     return sin;
 }
+
+
+/**
+ * Send the user request to the server.
+ * @param clientSocket The client's socket.
+ */
+void ClientSocket::sendToServer(int sock) {
+    // Getting the user request.
+    string message = this->userVectorInput.userInputFlow();
+    // Making the buffer ready.
+    const char *char_message = message.c_str();
+    // Checking the buffer size.
+    int data_len = strlen(char_message);
+    // Sending the data to the server.
+    int sentBytes = send(sock, char_message, data_len, 0);
+    // Checking if it sent correctly.
+    if (sentBytes < 0) {
+        perror("error sending the message to the server");
+    }
+}
+
+/**
+ * Recieved the data from the server.
+ * @param clientSocket The client's socket.
+ * @return The server reply.
+ */
+string ClientSocket::receiveData(int clientSocket) {
+    // Declaration of the buffer and the serverReply.
+    string serverReply;
+    char buffer[4096];
+    // Set the data that the server can accept from a connection to the buffer's size.
+    int expected_data_len = sizeof(buffer);
+    // Receive data from the current client.
+    long readBytes = recv(clientSocket, buffer, expected_data_len, 0);
+    // Check if the "receive" method worked.
+    if (readBytes < 0) {
+        // If it didn't work, raise an error.
+        perror("Error receiving data.");
+    }
+    // Check for an empty message.
+    if (readBytes == 0) {
+        close(clientSocket);
+        return "The connection is close";
+    }
+    // concatenating the server reply to a string.
+    for (int i = 0; i < readBytes; i++) {
+        serverReply += buffer[i];
+    }
+    return serverReply;
+}
+
+
+
 
 
--- Client/ClientSocket.h
+++ Client/ClientSocket.h
@@ -23,18 +23,32 @@ public:
     // The Destructor of the class.
     ~ClientSocket();
 
+    // Setter for the ip of the server.
     void setIp(string ip1);
 
+    // Setter for the port number of the server.
     void setPort(int portNumber);
 
+    // Getter for the ip of the server.
     string getIp();
 
+    // Getter for the port number of the server.
     int getPort();
 
+    // Create a socket with the server details.
     int creatClientSocket();
 
+    // Sending data to the server.
+    void sendToServer(int sock);
+
+    // Getting the server reply.
+    string receiveData(int clientSocket);
+
 protected:
+    // Create a socket.
     int makeNewSocket();
+
+    // Create the struct needed for creating a socket.
     sockaddr_in creatAddrInStruct();
 
 
@@ -42,7 +56,6 @@ private:
     string ip;
     int port;
     UserVectorInput userVectorInput;
-
 
 };
 
